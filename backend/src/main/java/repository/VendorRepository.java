8ff05f5c78e2a9b1598c4cd7bc0b519fead6163b
gangotrisd <gangotrisanjukumar@gmail.com>
2026-05-04 17:19:14 +0530

    Initial commit - vendor offboarding project

--- backend/src/main/java/repository/VendorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.internship.tool.repository;
-
-import com.internship.tool.model.Vendor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VendorRepository extends JpaRepository<Vendor, Long> {
-}
